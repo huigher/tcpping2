3d07e6e0d011f2a607aaf7e2c934cf82513ebd1d
霄翎 <sumin.lsm@alibaba-inc.com>
2017-11-21 23:30:33 +0800

    1、添加结果统计功能，类似linux ping的结果统计 2、添加日志大小约束（默认每个文件10M，供5个文件），防止撑爆磁盘

--- tcpping2.py
+++ tcpping2.py
@@ -6,6 +6,8 @@ import time
 import socket
 import struct
 import logging
+import logging.handlers
+import signal
 from datetime import datetime
 
 __VERSION__ = '0.1.0'
@@ -28,6 +30,7 @@ def conn_tcp(dst_host, dst_port, timeout, src_host=None, src_port=None, rst=Fals
     :param timeout: wait TIMEOUT second in connection period
     :return: (conn_time, close_time, err),connection time,close time and error message
     """
+
     (t1, t2, t3, te, conn_time, close_time, err, local_addr) = (-1, -1, -1, -1, -1, -1, '', None)
     # 若指定了RST参数，那么开始设定相关参数
     if rst:
@@ -84,7 +87,7 @@ def judge_args(argument):
     if bool(argument.src_host) ^ (bool(argument.src_port) or bool(argument.src_rotate_port)):
         tip = 'src_host and src_port(src_rotate_port) must be given at the same time'
         print tip
-        logging.error(tip)
+        mylogger.error(tip)
         return False
     else:
         return True
@@ -99,6 +102,7 @@ def go(dst_host, dst_port, timeout, interval, src_host=None, src_port=None, coun
     while judge_count(count):
         (conn_time, close_time, err, local_addr) = conn_tcp(dst_host, dst_port, timeout=timeout, src_host=src_host,
                                                             src_port=src_port, rst=rst)
+        result.put(conn_time, True if len(str(err)) == 0 else False)
         # 初始化存放输出信息的列表
         output = list()
         if local_addr:
@@ -114,11 +118,11 @@ def go(dst_host, dst_port, timeout, interval, src_host=None, src_port=None, coun
             output.append('ERROR: ' + str(err))
 
         final_output = ', '.join(output)
-        print final_output
+        # print final_output
         if error_flag:
-            logging.error(final_output)
+            mylogger.error(final_output)
         else:
-            logging.info(final_output)
+            mylogger.info(final_output)
 
         # 清除错误标志，执行本次循环的收尾工作
         err = ''
@@ -131,7 +135,7 @@ def go(dst_host, dst_port, timeout, interval, src_host=None, src_port=None, coun
         if src_port >= 65536:
             tip_reach_65535 = 'Local port reach 65535,reset src port to 1024.'
             print tip_reach_65535
-            logging.warning(tip_reach_65535)
+            mylogger.warning(tip_reach_65535)
             src_port = 1024
 
         # 若有 count，自减1
@@ -143,11 +147,14 @@ def go(dst_host, dst_port, timeout, interval, src_host=None, src_port=None, coun
 
 
 def initial(arguments):
+    signal.signal(signal.SIGINT, my_exit)
+    signal.signal(signal.SIGTERM, my_exit)
+
     # 开启log记录
-    logging.basicConfig(level=logging.DEBUG,
-                        format='%(levelname)s: %(asctime)s - %(filename)s[line:%(lineno)d] -  %(message)s',
-                        filename='tcpping2_' + arguments.dst_host[0] + '_' + str(arguments.dst_port[0]) + '.log',
-                        filemode='w')
+    # logging.basicConfig(level=logging.DEBUG,
+    #                     format='%(levelname)s: %(asctime)s - %(filename)s[line:%(lineno)d] -  %(message)s',
+    #                     filename='tcpping2_' + arguments.dst_host[0] + '_' + str(arguments.dst_port[0]) + '.log',
+    #                     filemode='w')
 
 
 def get_version():
@@ -185,8 +192,83 @@ def getargs():
     return parser.parse_args()
 
 
+def my_exit(signum, frame):
+    result_string = result.get_statistics()
+    mylogger.info(result_string)
+    exit()
+
+
+class ResultBucket:
+    is_initialled = False
+    ok_count = 0
+    error_count = 0
+    min_time = 0.0
+    max_time = 0.0
+    avg_time = 0.0
+
+    def __init__(self, dst_host, dst_port):
+        self.dst_host = dst_host
+        self.dst_port = dst_port
+        pass
+
+    def put(self, conn_time, status):
+        if status == False:
+            self.error_count += 1
+        else:
+            if self.is_initialled == False:
+                self.min_time = self.max_time = self.avg_time = conn_time
+                self.is_initialled = True
+            else:
+                if conn_time < self.min_time:
+                    self.min_time = conn_time
+                if conn_time > self.max_time:
+                    self.max_time = conn_time
+                self.avg_time = (self.avg_time * self.ok_count + conn_time) / (self.ok_count + 1)
+
+            self.ok_count += 1
+
+    def get_statistics(self):
+        format_string = """--- {dst_host}:{dst_port} tcpping statistics ---
+{total_count} packets transmitted, {ok_count} connected, {error_rate}% connection failed
+min/avg/max = {min_time}/{avg_time}/{max_time} ms"""
+
+        format_dict = dict()
+        format_dict['dst_host'] = self.dst_host
+        format_dict['dst_port'] = self.dst_port
+        format_dict['total_count'] = self.ok_count + self.error_count
+        format_dict['ok_count'] = self.ok_count
+        format_dict['error_rate'] = self.error_count / (self.ok_count + self.error_count) * 100
+        format_dict['min_time'] = str('%.6f' % self.min_time)
+        format_dict['avg_time'] = str('%.6f' % self.avg_time)
+        format_dict['max_time'] = str('%.6f' % self.max_time)
+        return format_string.format(**format_dict)
+
+
 if __name__ == '__main__':
     args = getargs()
+    result = ResultBucket(args.dst_host[0], args.dst_port[0])
+
+    # 设置输出的日志格式
+    console_formatter = logging.Formatter('%(message)s')
+    file_formatter = logging.Formatter('%(levelname)s: %(asctime)s - %(filename)s[line:%(lineno)d] -  %(message)s')
+
+    # 设置输出到屏幕的handler
+    console_handler = logging.StreamHandler()
+    console_handler.setFormatter(console_formatter)
+
+    # 设置输出到文件的handler
+    file_handler = logging.handlers.RotatingFileHandler(
+        'tcpping2_' + args.dst_host[0] + '_' + str(args.dst_port[0]) + '.log', mode='w',
+        maxBytes=10*1024*1024, backupCount=5)
+    file_handler.setFormatter(file_formatter)
+
+    # 创建logging
+    mylogger = logging.getLogger('tcpping2')
+    mylogger.setLevel(logging.INFO)
+    # 加入两个handler
+    mylogger.addHandler(console_handler)
+    mylogger.addHandler(file_handler)
+
     initial(args)
     if judge_args(args):
         go(args.dst_host[0], args.dst_port[0], timeout=args.timeout if args.timeout else 10,
@@ -194,5 +276,6 @@ if __name__ == '__main__':
            src_host=args.src_host if args.src_host else None, src_port=args.src_port if args.src_port else 0,
            src_rotate_port=args.src_rotate_port if args.src_rotate_port else None, rst=args.rst if args.rst else None,
            count=args.count if args.count else None)
+        print result.get_statistics()
     else:
         pass
